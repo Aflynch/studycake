485c6cc2ada6b78ec2ae5adce121581459e4e93a
Tacksoo Im <tacksoo.im@gmail.com>
2013-05-18 13:32:21 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studycake::Application.config.secret_token = '7fd4a51e6ec9bbeee25fd6cc6b67e6569244465462d9e7252615fc5e7e4afc5d7988045465cc82efa2b598dd9c4db17f282e1a0b8a2d491e9bd80442118d0b5d'
